bd2b151fa6983ff6ce3b14e79ca53b317fca0c20
RK Dhakal <40695193+rkdhakal@users.noreply.github.com>
2024-11-15 16:09:53 -0500

    Add files via upload

---
+++ retail.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import time
+print("Model Running .........")
+time.sleep(3)
+print(".................")
+time.sleep(3)
+print("Price Optimization for Retail Industry Successfully hosted")
